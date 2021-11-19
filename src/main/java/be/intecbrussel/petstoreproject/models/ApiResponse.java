3d5fb4012946b4852d51d83e2d0133ace02b33b5
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-19 10:49:28 +0100

    initial commit

--- src/main/java/be/intecbrussel/petstoreproject/models/ApiResponse.java
+++ src/main/java/be/intecbrussel/petstoreproject/models/ApiResponse.java
@@ -1,4 +1,19 @@
 package be.intecbrussel.petstoreproject.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ApiResponse {
+    private Integer code;
+    private String type;
+    private String message;
 }
