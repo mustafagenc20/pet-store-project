cb7aadc61dba6e35856e68c1d9b1ac41b35908bc
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-19 10:49:37 +0100

    initial commit

---
+++ src/main/java/be/intecbrussel/petstoreproject/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package be.intecbrussel.petstoreproject.repository;
+
+import be.intecbrussel.petstoreproject.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository <Category, Long> {
+}
