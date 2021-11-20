6b155289f6904d51e4d8949b359664e1788841dd
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-20 23:37:27 +0100

    initial commit

--- src/main/java/be/intecbrussel/petstoreproject/controller/PetController.java
+++ src/main/java/be/intecbrussel/petstoreproject/controller/PetController.java
@@ -1,52 +1,87 @@
 package be.intecbrussel.petstoreproject.controller;
 
 import be.intecbrussel.petstoreproject.models.ApiResponse;
-import be.intecbrussel.petstoreproject.models.Pet;
-import be.intecbrussel.petstoreproject.repository.PetRepository;
+import be.intecbrussel.petstoreproject.models.entity.Pet;
 import be.intecbrussel.petstoreproject.service.PetService;
-import be.intecbrussel.petstoreproject.service.PetStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/pet")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class PetController {
 
     private final PetService petService;
 
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @PostMapping
     public ResponseEntity createPet(@RequestBody Pet pet) {
-        Pet savedPet = petService.createPet(pet);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedPet);
+        if (pet == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(405, "invalid input", "invalid input"));
+        } else {
+            petService.createPet(pet);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(200, "OK", "Pet saved to database"));
+        }
     }
 
     @PutMapping
-    public ResponseEntity updatePet(@RequestBody Long id, Pet pet) {
-        Pet updatedPet = petService.updatePet(id, pet);
-        return ResponseEntity.status(HttpStatus.CREATED).body(updatedPet);
+    public ResponseEntity updatePet(@RequestBody Pet pet) {
+        if (petService.findPetById(pet.getId()) == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(400, "invalid ID supplied", "invalid ID supplied"));
+        } else {
+            petService.updatePet(pet);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(200, "OK", "Pet updated to the database"));
+        }
     }
 
-    @GetMapping("/findById")
+    @GetMapping("findById")
     public ResponseEntity getPetById(@RequestParam("id") Long id) {
-        Pet pet = petService.findPetById(id);
-        return ResponseEntity.status(HttpStatus.FOUND).body(pet);
+        if (id == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(400, "invalid ID supplied", "invalid ID supplied"));
+        } else {
+            Pet pet = petService.findPetById(id);
+            if (pet == null){
+                return ResponseEntity.status(HttpStatus.FOUND).body(new ApiResponse(404, "not found", " pet not found."));
+            } else {
+                return ResponseEntity.status(HttpStatus.OK).body(pet);
+            }
+        }
     }
 
-    @GetMapping("/findByStatus")
+    @GetMapping("findByStatus")
     public ResponseEntity getPetByStatus(@RequestParam("status") String status) {
-        Pet pet = petService.findPetByStatus(status);
-        return ResponseEntity.status(HttpStatus.FOUND).body(pet);
+        if (status == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(400,
+                    "Invalid status value",
+                    "Invalid status value"));
+        } else {
+            Pet pet = petService.findPetByStatus(status);
+            if (pet == null){
+                return ResponseEntity.status(HttpStatus.FOUND).body(new ApiResponse(404, "not found", " pet not found."));
+            } else {
+                return ResponseEntity.status(HttpStatus.OK).body(pet);
+            }
+        }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity deletePetById(@PathVariable("id") Long id) {
-        petService.deletePetById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        if (id == null) {
+            return ResponseEntity.badRequest().body(new ApiResponse(404,
+                    "Invalid ID supplied",
+                    "Invalid ID supplied"));
+        } else if (petService.findPetById(id) == null) {
+            return ResponseEntity.status(HttpStatus.FOUND).body(new ApiResponse(404, "not found", " pet not found."));
+        }
+        else {
+            petService.deletePetById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(200,"OK","Pet deleted from the database"));
+        }
     }
 }
 
--- src/main/java/be/intecbrussel/petstoreproject/repository/OrderRepository.java
+++ src/main/java/be/intecbrussel/petstoreproject/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package be.intecbrussel.petstoreproject.repository;
 
-import be.intecbrussel.petstoreproject.models.Order;
+import be.intecbrussel.petstoreproject.models.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+import java.util.Optional;
+
+//@Repository
+public interface OrderRepository { //extends JpaRepository<Order, Long> {
+    Order save(Order order);
+    Optional<Order> findById(Long id);
+    Optional<Order> findByStatus(String status);
+    void delete(Long id);
 }
