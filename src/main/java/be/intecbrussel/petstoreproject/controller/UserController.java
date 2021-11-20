75d51c782e880f2f42cec93f28ab6bd8bc7a7f52
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-20 23:38:16 +0100

    initial commit

--- src/main/java/be/intecbrussel/petstoreproject/controller/UserController.java
+++ src/main/java/be/intecbrussel/petstoreproject/controller/UserController.java
@@ -1,14 +1,11 @@
 package be.intecbrussel.petstoreproject.controller;
 
-import be.intecbrussel.petstoreproject.models.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/user")
+//@RestController
+//@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 //    @Autowired
--- src/main/java/be/intecbrussel/petstoreproject/repository/UserRepository.java
+++ src/main/java/be/intecbrussel/petstoreproject/repository/UserRepository.java
@@ -1,7 +1,16 @@
 package be.intecbrussel.petstoreproject.repository;
 
-import be.intecbrussel.petstoreproject.models.User;
+import be.intecbrussel.petstoreproject.models.entity.Order;
+import be.intecbrussel.petstoreproject.models.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.ArrayList;
+import java.util.List;
+
+public interface UserRepository{ // extends JpaRepository<User, Long> {
+/*
+    Order save(Order order);
+    List<Order> findById(Long id);
+    List<Order> findByStatus(String status);
+    void delete(Long id);*/
 }
