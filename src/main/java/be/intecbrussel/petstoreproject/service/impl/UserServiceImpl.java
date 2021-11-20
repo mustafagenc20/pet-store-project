0dd3547c5e87827de7d26822bcd4dff85e9a9bb0
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-20 12:51:38 +0100

    initial commit

--- src/main/java/be/intecbrussel/petstoreproject/service/impl/UserServiceImpl.java
+++ src/main/java/be/intecbrussel/petstoreproject/service/impl/UserServiceImpl.java
@@ -7,4 +7,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    @Override
+    public User createUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User createWithArray(ArrayList id) {
+        return null;
+    }
+
+    @Override
+    public User createWithList(List id) {
+        return null;
+    }
+
+    @Override
+    public User getLogin(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public User getLogout() {
+        return null;
+    }
+
+    @Override
+    public User updateUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public void deleteUsername(String username) {
+
+    }
 }
