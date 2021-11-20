41f889fae9a16cccf2f0b456c5462272f3f11492
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-20 12:51:20 +0100

    initial commit

--- src/main/java/be/intecbrussel/petstoreproject/repository/UserRepository.java
+++ src/main/java/be/intecbrussel/petstoreproject/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package be.intecbrussel.petstoreproject.repository;
 
-public interface UserRepository {
+import be.intecbrussel.petstoreproject.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
