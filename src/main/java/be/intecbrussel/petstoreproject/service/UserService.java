72a495934d11f941b905212b57fa327b21320bdb
Mustafa Genc <mustafagenc20@gmail.com>
2021-11-20 23:38:38 +0100

    initial commit

--- src/main/java/be/intecbrussel/petstoreproject/service/UserService.java
+++ src/main/java/be/intecbrussel/petstoreproject/service/UserService.java
@@ -1,17 +1,16 @@
 package be.intecbrussel.petstoreproject.service;
 
-import be.intecbrussel.petstoreproject.models.Pet;
-import be.intecbrussel.petstoreproject.models.User;
+import be.intecbrussel.petstoreproject.models.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public interface UserService {
-    User createUser(User user);
+/*    User createUser(User user);
     User createWithArray(ArrayList id);
     User createWithList(List id);
     User getLogin(String username, String password);
     User getLogout();
     User updateUsername(String username);
-    void deleteUsername(String username);
+    void deleteUsername(String username);*/
 }
